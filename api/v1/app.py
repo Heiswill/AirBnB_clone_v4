9f5b0f930064f0ee1f9694a35a6dd0e1b02de5df
Heiswill <willtriger@gmail.com>
2023-01-28 17:18:43 +0000

    out

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,13 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
 port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else 5000
 
